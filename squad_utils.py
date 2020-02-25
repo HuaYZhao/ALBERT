2fab73c6c1c525cf28c20862efc2efb65d3f8bbf
HuaYZhao <921203405@qq.com>
2020-02-25 17:23:00 +0800

    rl loss: fix

--- squad_utils.py
+++ squad_utils.py
@@ -1984,9 +1984,9 @@ def v2_model_fn_builder(albert_config, init_checkpoint, learning_rate,
             #                   features["end_positions"], sample_num=4)
 
             from rl.rl_loss2 import rl_loss
-            logits = project_encoder_layers(outputs, features, project_layers_num=4)
+            logits = project_encoder_layers(outputs, features, project_layers_num=1)
             loss_rl = rl_loss(logits, features["start_positions"], features["end_positions"],
-                              project_layers_num=4, sample_num=4)
+                              project_layers_num=1, sample_num=1)
 
             # total_loss += loss_rl * 0.5
             theta_ce = tf.get_variable('theta_ce', (), tf.float32)
