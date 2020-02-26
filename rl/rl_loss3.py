e2c19ed561f90bcf4152b7ec525cc53e9cf6976d
HuaYZhao <921203405@qq.com>
2020-02-26 09:58:07 +0800

    up

---
+++ rl/rl_loss3.py
@@ -0,0 +1,126 @@
+import tensorflow as tf
+from rl.utils import mask_to_start
+
+
+def simple_tf_f1_score(tensors):
+    prediction_start = tf.cast(tensors[0], dtype=tf.float32)
+    prediction_end = tf.cast(tensors[1], dtype=tf.float32)
+    ground_truth_start = tf.cast(tensors[2], dtype=tf.float32)
+    ground_truth_end = tf.cast(tensors[3], dtype=tf.float32)
+
+    min_end = tf.reduce_min([prediction_end, ground_truth_end])
+    max_start = tf.reduce_max([prediction_start, ground_truth_start])
+
+    overlap = tf.cond(tf.greater(max_start, min_end), lambda: 0., lambda: min_end - max_start + 1)
+    precision = tf.cond(tf.equal(overlap, 0.), lambda: 0., lambda: overlap / (prediction_end - prediction_start + 1))
+    recall = tf.cond(tf.equal(overlap, 0.), lambda: 1e-20,
+                     lambda: overlap / (ground_truth_end - ground_truth_start + 1))
+
+    f1 = (2 * precision * recall) / (precision + recall)
+    return f1
+
+
+def reward(guess_start, guess_end, answer_start, answer_end, baseline, project_layers_num, sample_num):
+    """
+    Reinforcement learning reward (i.e. F1 score) from sampling a trajectory of guesses across each decoder timestep
+    """
+    reward = [[]] * sample_num
+
+    print("answer_start_shape:", answer_start.shape)
+    answer_start = tf.tile(answer_start, [project_layers_num])
+    answer_end = tf.tile(answer_end, [project_layers_num])
+    baseline = tf.tile(baseline, [project_layers_num])
+
+    for t in range(sample_num):
+        f1_score = tf.map_fn(
+            simple_tf_f1_score, (guess_start[:, t], guess_end[:, t], answer_start, answer_end),
+            dtype=tf.float32)  # [bs,]
+        normalized_reward = tf.stop_gradient(f1_score - baseline)
+        reward[t] = normalized_reward
+    return tf.stack(reward, axis=-1)  # [bs * project_layers_num, sample]
+
+
+# def surrogate_loss(logits, guess_start, guess_end, r, project_layers_num, sample_num):
+#     """
+#     The surrogate loss to be used for policy gradient updates
+#     """
+#
+#     logits = tf.concat(logits, axis=0) # [16, ...]
+#     loss = 0.
+#
+#     for _sample_i in range(sample_num):
+#         guess_start_i = guess_start[:, _sample_i]
+#         guess_end_i = guess_end[:, _sample_i]
+#         r_i = r[:, _sample_i]
+#         start_loss = r_i * \
+#                      tf.nn.sparse_softmax_cross_entropy_with_logits(
+#                          logits=logits[:, :, 0], labels=guess_start_i)
+#         end_loss = r_i * \
+#                    tf.nn.sparse_softmax_cross_entropy_with_logits(
+#                        logits=logits[:, :, 1], labels=guess_end_i)
+#         loss += tf.reduce_mean(start_loss + end_loss)
+#
+#     return loss
+def surrogate_loss(logits, guess_start, guess_end, r, project_layers_num, sample_num):
+    """
+    The surrogate loss to be used for policy gradient updates
+    """
+    bs = logits[0].shape.as_list()[0]
+
+    logits = tf.concat(logits, axis=0)  # [16, ...]
+    guess_start = tf.reshape(guess_start, [-1])  # (bs * simple_num ,)
+    guess_end = tf.reshape(guess_end, [-1])
+    r = tf.reshape(r, [-1])
+    start_logits = tf.concat(
+        [tf.tile(_sp, [sample_num, 1]) for _sp in tf.split(logits[:, :, 0], bs * project_layers_num)], axis=0)
+    end_logits = tf.concat(
+        [tf.tile(_sp, [sample_num, 1]) for _sp in tf.split(logits[:, :, 1], bs * project_layers_num)], axis=0)
+    start_loss = r * \
+                 tf.nn.sparse_softmax_cross_entropy_with_logits(
+                     logits=start_logits, labels=guess_start)
+    end_loss = r * \
+               tf.nn.sparse_softmax_cross_entropy_with_logits(
+                   logits=end_logits, labels=guess_end)
+    print(start_loss.shape)
+    start_loss = tf.stack(tf.split(start_loss, sample_num), axis=1)
+    end_loss = tf.stack(tf.split(end_loss, sample_num), axis=1)
+    loss = tf.reduce_mean(tf.reduce_mean(
+        start_loss + end_loss, axis=1), axis=0)
+    return loss
+
+
+def rl_loss(logits, answer_start, answer_end, project_layers_num=4, sample_num=1):
+    """
+    Reinforcement learning loss
+    """
+    final_logits = logits[project_layers_num - 1]
+    final_start_logits = final_logits[:, :, 0]
+    final_end_logits = final_logits[:, :, 1]
+    guess_start_greedy = tf.argmax(final_start_logits, axis=1)
+    # end_logits = tf.argmax(mask_to_start(
+    #     end_logits, guess_start_greedy), axis=1)
+    guess_end_greedy = tf.argmax(final_end_logits, axis=1)
+    baseline = tf.map_fn(simple_tf_f1_score, (guess_start_greedy, guess_end_greedy,
+                                              answer_start, answer_end), dtype=tf.float32)
+
+    guess_start = []
+    guess_end = []
+
+    for t in range(project_layers_num):
+        logits_t = logits[t]
+        start_logits = logits_t[:, :, 0]
+        end_logits = logits_t[:, :, 1]
+        guess_start.append(tf.multinomial(start_logits, sample_num))
+        guess_end.append(tf.multinomial(end_logits, sample_num))
+    guess_start = tf.concat(guess_start, axis=0)
+    guess_end = tf.concat(guess_end, axis=0)
+    print("guess_start_shape", guess_start.shape)
+    r = reward(guess_start, guess_end, answer_start, answer_end, baseline, project_layers_num,
+               sample_num)  # [bs*project_layers,4]
+    # print("reward_shape:", r.shape)
+    surr_loss = surrogate_loss(logits, guess_start, guess_end, r, project_layers_num, sample_num)
+    loss = tf.reduce_mean(-r)
+
+    # This function needs to return the value of loss in the forward pass so that theta_rl gets the right parameter update
+    # However, this needs to have the gradient of surr_loss in the backward pass so the model gets the right policy gradient update
+    return surr_loss + tf.stop_gradient(loss - surr_loss)
