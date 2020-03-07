373713b0d911b3610a614f8de9eb5c416bb3df53
huayangzhao <921203405@qq.com>
2020-03-07 20:55:43 +0800

    oom

--- squad_utils.py
+++ squad_utils.py
@@ -1739,7 +1739,9 @@ def v2_model_fn_builder(albert_config, init_checkpoint, learning_rate,
 
             adv_loss = get_loss(outputs_adv, features)
 
-            total_loss = total_loss * 0.875 + adv_loss * 0.125
+            # total_loss = total_loss * 0.875 + adv_loss * 0.125
+            total_loss = adv_loss
+
             train_op = optimization.create_optimizer(
                 total_loss, learning_rate, num_train_steps, num_warmup_steps, use_tpu)
 
