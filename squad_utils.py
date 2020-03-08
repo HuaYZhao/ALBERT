36c889d9ffe41460f4db9a21030c7ed043dc9a26
huayangzhao <921203405@qq.com>
2020-03-08 15:29:28 +0800

    oom

--- squad_utils.py
+++ squad_utils.py
@@ -1626,7 +1626,7 @@ def v2_model_fn_builder(albert_config, init_checkpoint, learning_rate,
         flat_input_ids = tf.reshape(input_ids, [-1])
         one_hot_input_ids = tf.one_hot(flat_input_ids, depth=vocab_size)  # [5*384, 30000]
         # 取扰动的embedding
-        if is_training and np.random.rand() < 0:
+        if is_training and np.random.rand() < 0.1:
             output = tf.matmul(one_hot_input_ids, perturb_embedding_table)
             input_shape = modeling.get_shape_list(input_ids)
             perturb_embedded_inputs = tf.reshape(output,
