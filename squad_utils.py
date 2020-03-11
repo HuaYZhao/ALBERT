1f827f91fe1d33b9446205756676cf984098df62
HuaYZhao <921203405@qq.com>
2020-03-11 16:06:07 +0800

    oom

--- squad_utils.py
+++ squad_utils.py
@@ -1771,8 +1771,9 @@ def v2_model_fn_builder(albert_config, init_checkpoint, learning_rate,
             total_loss = total_loss * 0.875 + adv_loss * 0.125
 
             train_op = optimization.create_optimizer(
-                total_loss, learning_rate, num_train_steps, num_warmup_steps, use_tpu,)
-                # growth_step=tf.equal(adv_step, 1))
+                total_loss, learning_rate, num_train_steps, num_warmup_steps, use_tpu,
+                growth_step=tf.constant(True, tf.bool))
+            # growth_step=tf.equal(adv_step, 1))
 
             # train_op = tf.group(train_op, perturb_assign_op, adv_assign_op)
 
