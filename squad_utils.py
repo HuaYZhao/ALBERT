6c7694b156bf06c8e7b6bae6dd70261bb31a4152
HuaYZhao <921203405@qq.com>
2020-03-12 09:36:57 +0800

    oom

--- squad_utils.py
+++ squad_utils.py
@@ -1762,10 +1762,10 @@ def v2_model_fn_builder(albert_config, init_checkpoint, learning_rate,
                 return gvs.values()
 
             def clear_collection():
-                tf.assert_equal(len(tf.get_collection("temp_gvs")), 1)
-                temp_gvs = tf.get_collection_ref("temp_gvs")[0]
-                gvs = {v: g + temp_gvs[v] for g, v in zip(grads, tvars)}
-                # del temp_gvs
+                with tf.control_dependencies([tf.assert_equal(len(tf.get_collection("temp_gvs")), 1)]):
+                    temp_gvs = tf.get_collection_ref("temp_gvs")[0]
+                    gvs = {v: g + temp_gvs[v] for g, v in zip(grads, tvars)}
+                    # del temp_gvs
                 return gvs.values()
 
             grads = tf.cond(tf.equal(adv_step, 0), save_to_collection, clear_collection)
