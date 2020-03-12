fd1f13bba3f6c1214a2d240adec49ef0dbce88d6
HuaYZhao <921203405@qq.com>
2020-03-12 17:02:09 +0800

    oom

--- squad_utils.py
+++ squad_utils.py
@@ -1867,8 +1867,9 @@ def v2_model_fn_builder(albert_config, init_checkpoint, learning_rate,
             # train_op = tf.cond(tf.equal(adv_step, 0), lambda: tf.no_op(),
             #                    lambda: optimization.create_optimizer(
             #                        list(zip(grads, tvars)), learning_rate, num_train_steps, num_warmup_steps, use_tpu))
-            train_op = optimization.create_optimizer(
-                                   list(zip(grads, tvars)), learning_rate, num_train_steps, num_warmup_steps, use_tpu)
+            train_op = tf.cond(tf.equal(adv_step, 1), lambda: tf.no_op(),
+                               lambda: optimization.create_optimizer(
+                                   list(zip(grads, tvars)), learning_rate, num_train_steps, num_warmup_steps, use_tpu))
 
             # with tf.control_dependencies([train_op]):
             #     adv_assign_op = tf.assign(adv_step, 1 - adv_step)
