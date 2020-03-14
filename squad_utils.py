842bc1f71f50de6ef8225191ca3cd688bbce3035
HuaYZhao <921203405@qq.com>
2020-03-14 11:19:48 +0800

    oom

--- squad_utils.py
+++ squad_utils.py
@@ -1843,20 +1843,36 @@ def v2_model_fn_builder(albert_config, init_checkpoint, learning_rate,
 
             grads = tf.gradients(total_loss, tvars)
 
+            # def save_grads():
+            #     # nonlocal grads
+            #     for i, v in enumerate(tvars):
+            #         g = tf.assign(before_grads[v], grads[i])
+            #         grads[i] = g
+            #     return grads
+            #
+            # def sum_grads():
+            #     # nonlocal grads
+            #     # new_grads = []
+            #     # for i, v in enumerate(tvars):
+            #     #     g = before_grads[v] + grads[i]
+            #     #     grads[i] = g
+            #     (grads, _) = tf.clip_by_global_norm(grads, clip_norm=1.0)
+            #     return grads
+
             def save_grads():
-                nonlocal grads
+                new_grads = []
                 for i, v in enumerate(tvars):
-                    g = tf.assign(before_grads[v], grads[i])
-                    grads[i] = g
-                return grads
+                    new_grad = tf.assign(before_grads[v], grads[i])
+                    new_grads.append(new_grad)
+                return new_grads
 
             def sum_grads():
-                nonlocal grads
+                new_grads = []
                 for i, v in enumerate(tvars):
-                    g = before_grads[v] + grads[i]
-                    grads[i] = g
-                (grads, _) = tf.clip_by_global_norm(grads, clip_norm=1.0)
-                return grads
+                    new_grad = grads[i] + before_grads[v]
+                    new_grads.append(new_grad)
+                (new_grads, _) = tf.clip_by_global_norm(new_grads, clip_norm=1.0)
+                return new_grads
 
             grads = tf.cond(tf.equal(adv_step, 0), save_grads, sum_grads)
 
