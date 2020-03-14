476ade82a71c8cbce52ae667f3e51d3aea33dd30
HuaYZhao <921203405@qq.com>
2020-03-14 12:44:56 +0800

    oom

--- squad_utils.py
+++ squad_utils.py
@@ -1861,21 +1861,21 @@ def v2_model_fn_builder(albert_config, init_checkpoint, learning_rate,
             #     (grads, _) = tf.clip_by_global_norm(grads, clip_norm=1.0)
             #     return grads
 
-            # def save_grads():
-            #     new_grads = []
-            #     for i, v in enumerate(tvars):
-            #         new_grads.append(tf.assign(before_grads[v], grads[i]))
-            #     return new_grads
-            #
-            # def sum_grads():
-            #     # new_grads = []
-            #     # for i, v in enumerate(tvars):
-            #     #     new_grad = grads[i] + before_grads[v]
-            #     #     new_grads.append(new_grad)
-            #     # (new_grads, _) = tf.clip_by_global_norm(new_grads, clip_norm=1.0)
-            #     return grads
-            #
-            # grads = tf.cond(tf.equal(adv_step, 0), save_grads, sum_grads)
+            def save_grads():
+                new_grads = []
+                for i, v in enumerate(tvars):
+                    new_grads.append(tf.assign(before_grads[v], grads[i]))
+                return new_grads
+
+            def sum_grads():
+                # new_grads = []
+                # for i, v in enumerate(tvars):
+                #     new_grad = grads[i] + before_grads[v]
+                #     new_grads.append(new_grad)
+                # (new_grads, _) = tf.clip_by_global_norm(new_grads, clip_norm=1.0)
+                return grads
+
+            grads = tf.cond(tf.equal(adv_step, 0), save_grads, sum_grads)
 
             # train_op = tf.cond(tf.equal(adv_step, 0), lambda: tf.no_op(),
             #                    lambda: optimization.create_optimizer(
