a3e541540766798fc63e999facbdbe93fd43114f
huayangzhao <921203405@qq.com>
2020-03-15 16:57:27 +0800

    oom

--- run_squad_v2.py
+++ run_squad_v2.py
@@ -553,8 +553,7 @@ def main(_):
             perturb = result["perturb"]
             perturb_dict[unique_id] = perturb
             if len(perturb_dict.keys()) > 1000:
-                with tf.gfile.Open(f"perturb_{_id}.json", "w") as perturb_file:
-                    json.dump(perturb_dict, perturb_file)
+                np.savez(f"perturb_{_id}.npz", **perturb_dict)
                 perturb_dict = {}
                 _id += 1
 
