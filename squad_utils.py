b79b1d294df2e97d1889f97963df0a3100b17db2
HuaYZhao <921203405@qq.com>
2020-03-19 09:56:45 +0800

    answer_model

--- squad_utils.py
+++ squad_utils.py
@@ -147,10 +147,9 @@ def read_squad_examples(input_file, is_training):
                 question_text = qa["question"]
                 start_position = None
                 orig_answer_text = None
-                is_impossible = False
+                is_impossible = qa.get("is_impossible", False)
 
                 if is_training:
-                    is_impossible = qa.get("is_impossible", False)
                     if (len(qa["answers"]) != 1) and (not is_impossible):
                         raise ValueError(
                             "For training, each question should have exactly 1 answer.")
