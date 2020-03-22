5c80d7fc55926841776f588946166e971366cc06
huayangzhao <921203405@qq.com>
2020-03-22 18:59:19 +0800

    bfloat

--- optimization.py
+++ optimization.py
@@ -179,7 +179,7 @@ class AdamWeightDecayOptimizer(tf.train.Optimizer):
             # Standard Adam update.
             print(grad.dtype)
             next_m = (
-                    tf.multiply(self.beta_1, m) + tf.multiply(1.0 - self.beta_1, tf.bfloat16, grad))
+                    tf.multiply(self.beta_1, m) + tf.multiply(1.0 - self.beta_1, grad))
             next_v = (
                     tf.multiply(self.beta_2, v) + tf.multiply(1.0 - self.beta_2,
                                                               tf.square(grad)))
