8ce5b4ae337dd9c2a4a9ce756036b4a2e1dfb48c
HuaYZhao <921203405@qq.com>
2020-03-24 19:14:40 +0800

    mp

--- squad_utils.py
+++ squad_utils.py
@@ -694,14 +694,14 @@ def input_fn_builder(input_file, seq_length, is_training,
             d = d.repeat()
             d = d.shuffle(buffer_size=1000)
 
-        d = d.map(map_func=lambda record: _decode_record(record, name_to_features))
-        d = d.batch(batch_size=batch_size, drop_remainder=drop_remainder)
+        # d = d.map(map_func=lambda record: _decode_record(record, name_to_features))
+        # d = d.batch(batch_size=batch_size, drop_remainder=drop_remainder)
 
-        # d = d.apply(
-        #     map_and_batch(
-        #         lambda record: _decode_record(record, name_to_features),
-        #         batch_size=batch_size,
-        #         drop_remainder=drop_remainder))
+        d = d.apply(
+            tf.data.Dataset.map_and_batch(
+                lambda record: _decode_record(record, name_to_features),
+                batch_size=batch_size,
+                drop_remainder=drop_remainder))
 
         return d
 
