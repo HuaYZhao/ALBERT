edefb400c0d23c97268c9a94b24780c3f47b7cb3
HuaYZhao <921203405@qq.com>
2020-04-11 15:58:45 +0800

    refine

--- external_verifier.py
+++ external_verifier.py
@@ -2,9 +2,10 @@ import json
 from copy import deepcopy
 import os
 from matplotlib import pyplot as plt
-from data.eval import get_raw_scores
+from data.eval import get_raw_scores, normalize_answer
 import numpy as np
 import time
+from functional import seq
 
 prediction_file = './data/predictions.json'
 prediction = json.load(open(prediction_file, 'r', encoding='utf-8'))
@@ -20,6 +21,12 @@ no_answer_prediction = json.load(open(no_answer_prediction_file, 'r', encoding='
 
 dev_file = './data/dev-v2.0.json'
 dev = json.load(open(dev_file, 'r', encoding='utf-8'))['data']
+
+train_file = './data/train-v2.0.json'
+train = json.load(open(train_file, 'r', encoding='utf-8'))['data']
+
+train_preds_file = './data/squad_preds.json'
+train_preds = json.load(open(train_preds_file, 'r', encoding='utf-8'))
 
 merge_prediction_file = './data/merge_predictions.json'
 
@@ -133,11 +140,72 @@ def simple_replace_with_null_odds():
           f"best_threshold: {best_threshold}")
 
 
-xargs = "python ./data/eval.py ./data/dev-v2.0.json ./data/predictions.json"
-os.system(xargs)
+def check_data():
+    dev_exact_scores, dev_f1_scores = get_raw_scores(dev, prediction)
+    train_exact_scores, train_f1_scores = get_raw_scores(train, train_preds)
 
-start = time.time()
-simple_replace_with_null_odds()
-xargs = "python ./data/eval.py ./data/dev-v2.0.json ./data/merge_predictions.json"
-os.system(xargs)
-print(f"cost time: {time.time() - start}")
+    train_error_exact = seq(train_exact_scores.items()).filter(lambda x: x[1] == 0).map(lambda x: x[0]).list()
+    dev_error_exact = seq(dev_exact_scores.items()).filter(lambda x: x[1] == 0).map(lambda x: x[0]).list()
+
+    all_dev_qas = seq(dev).map(lambda x: x['paragraphs']).flatten().list()
+    all_train_qas = seq(train).map(lambda x: x['paragraphs']).flatten().list()
+
+    def find_id(id_, all_qas):
+        for qas in all_qas:
+            qa = seq(qas['qas']).filter(lambda x: x['id'] == id_).list()
+            if qa:
+                return dict(context=qas['context'],
+                            prediction=prediction.get(id_, None) or train_preds.get(id_, None),
+                            **qa[0])
+
+    dev_error_exact_qas = [find_id(id_, all_dev_qas) for id_ in dev_error_exact[:100]]
+    train_error_exact_qas = [find_id(id_, all_train_qas) for id_ in train_error_exact[:100]]
+
+    # x = seq(dev).map(lambda x: [x['title'], get_raw_scores([x], prediction)]).map(
+    #     lambda x: [x[0], np.mean(list(x[1][0].values())), np.mean(list(x[1][1].values()))]).list()
+    print(1)
+
+
+def write_answer_refine():
+    answer_refine_file = './data/answer_refine.meta'
+
+    answer_refine_class = {}
+    for article in train:
+        for p in article['paragraphs']:
+            for qa in p['qas']:
+                info = {}
+                qid = qa['id']
+                info['pred'] = train_preds[qid]
+
+                gold_answers = [a['text'] for a in qa['answers']
+                                if normalize_answer(a['text'])]
+                assert len(gold_answers) <= 1, gold_answers
+                if not gold_answers:
+                    info['answer'] = ""
+                    info['class'] = 0
+                    answer_refine_class[qid] = info
+                    continue
+                gold_answer = gold_answers[0]
+                info['answer'] = gold_answer
+                pred = train_preds[qid]
+                if pred == gold_answer:
+                    info['class'] = 0
+                elif pred in gold_answer:
+                    info['class'] = 1
+                elif gold_answer in pred:
+                    info['class'] = 2
+                else:
+                    info['class'] = 0
+                answer_refine_class[qid] = info
+    json.dump(answer_refine_class, open(answer_refine_file, 'w', encoding='utf-8'))
+
+
+# xargs = "python ./data/eval.py ./data/dev-v2.0.json ./data/predictions.json"
+# os.system(xargs)
+
+# start = time.time()
+# simple_replace_with_null_odds()
+# xargs = "python ./data/eval.py ./data/dev-v2.0.json ./data/merge_predictions.json"
+# os.system(xargs)
+# print(f"cost time: {time.time() - start}")
+write_answer_refine()
